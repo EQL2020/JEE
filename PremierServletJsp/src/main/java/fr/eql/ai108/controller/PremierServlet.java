9c051a00aa98172791627493a277d482b6863fd8
EQL2020 <eqlrepository@gmail.com>
2021-02-04 17:29:01 +0100

    1ère, 2ème et 3ème servlet

--- PremierServletJsp/src/main/java/fr/eql/ai108/controller/PremierServlet.java
+++ PremierServletJsp/src/main/java/fr/eql/ai108/controller/PremierServlet.java
@@ -31,6 +31,22 @@ public class PremierServlet extends HttpServlet{
 		out.println("</head>");
 		out.println("<body>");
 		out.println("<h1>Hello AI108 !!!</h1>");
+		out.println("<form method='POST' action='SecondServlet'>");
+		out.println("Login: <input type='text' name='login'/><br/>");
+		out.println("Password: <input type='password' name='password'/><br/> ");
+		out.println("Sexe: <input type='radio' name='sexe' value='F'/> Femme");
+		out.println("<input type='radio' name='sexe' value='H'/> Homme");
+		out.println("<input type='radio' name='sexe' value='A'/> Autre <br/>");
+		out.println("Pays: <select name='pays'>");
+		out.println("<option value='--'> Choisir...</option>");
+		out.println("<option value='France'>France</option>");
+		out.println("<option value='Burkina Faso'>Burkina Faso</option>");
+		out.println("<option value='Egypte'>Egypte</option>");
+		out.println("</select><br/>");
+		out.println("<input type='checkbox' name='optin'/>"
+				+ " J'accepte les conditions d'utilisation <br/>");
+		out.println("<input type='submit' value='Valider'/>");
+		out.println("</form>");
 		out.println("</body>");
 		out.println("</html>");
 	}
