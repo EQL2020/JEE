95ab8a761ae345c1466c3448571e297e460aaad0
EQL2020 <eqlrepository@gmail.com>
2021-02-12 15:35:08 +0100

    PremierStruts

---
+++ PremierStruts/src/main/java/fr/eql/ai108/struts/action/PremierAction.java
@@ -0,0 +1,27 @@
+package fr.eql.ai108.struts.action;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import org.apache.struts.action.Action;
+import org.apache.struts.action.ActionForm;
+import org.apache.struts.action.ActionForward;
+import org.apache.struts.action.ActionMapping;
+
+import fr.eql.ai108.struts.actionform.PremierActionForm;
+
+public class PremierAction extends Action{
+	
+	public ActionForward execute(
+			ActionMapping paramMapping,
+			ActionForm paramForm,
+			HttpServletRequest paramRequest,
+			HttpServletResponse paramResponse) {
+		String forward = "success";
+		PremierActionForm form = (PremierActionForm) paramForm;
+		form.setLabel(form.getSaisie());
+		
+		return paramMapping.findForward(forward);
+	}
+
+}
