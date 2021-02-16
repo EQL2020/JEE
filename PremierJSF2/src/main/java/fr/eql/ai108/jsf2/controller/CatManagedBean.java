07c8333f5652dea01f497245a2e593347ed6fde2
EQL2020 <eqlrepository@gmail.com>
2021-02-16 14:08:57 +0100

    Affichage faces-message

--- PremierJSF2/src/main/java/fr/eql/ai108/jsf2/controller/CatManagedBean.java
+++ PremierJSF2/src/main/java/fr/eql/ai108/jsf2/controller/CatManagedBean.java
@@ -4,11 +4,13 @@ import java.util.List;
 
 import javax.annotation.PostConstruct;
 import javax.faces.bean.ManagedBean;
+import javax.faces.bean.ManagedProperty;
 import javax.faces.bean.RequestScoped;
 
 import fr.eql.ai108.dao.Cat;
 import fr.eql.ai108.dao.DaoCat;
 import fr.eql.ai108.dao.IDaoCat;
+import fr.eql.ai108.dao.User;
 
 @ManagedBean (name = "mbCat")
 @RequestScoped
@@ -16,7 +18,14 @@ public class CatManagedBean {
 	
 	private List<Cat> cats;
 	private IDaoCat dao = new DaoCat();
+	private List<Cat> connectedUsersCats;
 	
+
+	@ManagedProperty (value = "#{mbConnect.user}")
+	private User userConnected;
+	
+
+
 	public String deleteCat(Cat cat) {
 		System.out.println(cat.toString());
 		dao.delete(cat);
@@ -26,6 +35,10 @@ public class CatManagedBean {
 	@PostConstruct
 	public void init() {
 		cats = dao.getAll();
+		if (userConnected != null) {
+			//charger en mémoire la liste de chat du user
+			connectedUsersCats = dao.getCatByUser(userConnected);
+		}
 	}
 
 	public List<Cat> getCats() {
@@ -35,5 +48,18 @@ public class CatManagedBean {
 	public void setCats(List<Cat> cats) {
 		this.cats = cats;
 	}
+	
+	public User getUserConnected() {
+		return userConnected;
+	}
+	public void setUserConnected(User userConnected) {
+		this.userConnected = userConnected;
+	}
+	public List<Cat> getConnectedUsersCats() {
+		return connectedUsersCats;
+	}
 
+	public void setConnectedUsersCats(List<Cat> connectedUsersCats) {
+		this.connectedUsersCats = connectedUsersCats;
+	}
 }
--- PremierJSF2/src/main/java/fr/eql/ai108/jsf2/controller/ConnexionManagedBean.java
+++ PremierJSF2/src/main/java/fr/eql/ai108/jsf2/controller/ConnexionManagedBean.java
@@ -2,8 +2,11 @@ package fr.eql.ai108.jsf2.controller;
 
 import java.io.Serializable;
 
+import javax.faces.application.FacesMessage;
 import javax.faces.bean.ManagedBean;
 import javax.faces.bean.SessionScoped;
+import javax.faces.context.FacesContext;
+import javax.servlet.http.HttpSession;
 
 import fr.eql.ai108.dao.DaoUser;
 import fr.eql.ai108.dao.IDaoUser;
@@ -25,8 +28,27 @@ public class ConnexionManagedBean implements Serializable{
 		user = dao.authenticate(login, password);
 		if (user != null) {
 			retour = "/pageConnectee.xhtml?faces-redirect=true";
+		}else {
+			System.out.println("Coucou Else");
+			FacesContext.getCurrentInstance()
+			.addMessage(null, new FacesMessage(FacesMessage.SEVERITY_WARN, 
+					"Login et/ou Password incorrect", "azertyuiop"));
+			FacesContext.getCurrentInstance().addMessage("form:password", 
+					new FacesMessage("Un message ciblé"));
 		}
 		return retour;
+	}
+	
+	public String disconnect() {
+		HttpSession session = (HttpSession) FacesContext
+				.getCurrentInstance().getExternalContext().getSession(true);
+		session.invalidate();
+		//Réinitialisation des champs:
+		login = "";
+		password = "";
+		user = new User();
+		
+		return "/connexion.xhtml?faces-redirect=true";
 	}
 	public String getLogin() {
 		return login;
